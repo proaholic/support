afe6c2709d74d3c1742971fcfafaa0f92bbfde1c
cestbon <734245503@qq.com>
2021-02-05 09:53:36 +0800

    update helpers

--- helpers.php
+++ helpers.php
@@ -9,176 +9,165 @@ namespace Chenpkg\Support;
 
 use Closure;
 
-if (! function_exists('head')) {
-    /**
-     * Get the first element of an array. Useful for method chaining.
-     *
-     * @param  array  $array
-     * @return mixed
-     */
-    function head($array)
-    {
-        return reset($array);
-    }
+
+/**
+ * Get the first element of an array. Useful for method chaining.
+ *
+ * @param  array  $array
+ * @return mixed
+ */
+function head($array)
+{
+    return reset($array);
 }
 
-if (! function_exists('last')) {
-    /**
-     * Get the last element from an array.
-     *
-     * @param  array  $array
-     * @return mixed
-     */
-    function last($array)
-    {
-        return end($array);
-    }
+/**
+ * Get the last element from an array.
+ *
+ * @param  array  $array
+ * @return mixed
+ */
+function last($array)
+{
+    return end($array);
 }
 
-if (! function_exists('value')) {
-    /**
-     * Return the default value of the given value.
-     *
-     * @param $value
-     * @return mixed
-     */
-    function value($value)
-    {
-        return $value instanceof Closure ? $value() : $value;
-    }
+/**
+ * Return the default value of the given value.
+ *
+ * @param $value
+ * @return mixed
+ */
+function value($value)
+{
+    return $value instanceof Closure ? $value() : $value;
 }
 
-if (! function_exists('data_get')) {
-    /**
-     * Get an item from an array or object using "dot" notation.
-     *
-     * @param  mixed  $target
-     * @param  string|array|int|null  $key
-     * @param  mixed  $default
-     * @return mixed
-     */
-    function data_get($target, $key, $default = null)
-    {
-        if (is_null($key)) {
+/**
+ * Get an item from an array or object using "dot" notation.
+ *
+ * @param  mixed  $target
+ * @param  string|array|int|null  $key
+ * @param  mixed  $default
+ * @return mixed
+ */
+function data_get($target, $key, $default = null)
+{
+    if (is_null($key)) {
+        return $target;
+    }
+
+    $key = is_array($key) ? $key : explode('.', $key);
+
+    foreach ($key as $i => $segment) {
+        unset($key[$i]);
+
+        if (is_null($segment)) {
             return $target;
         }
 
-        $key = is_array($key) ? $key : explode('.', $key);
-
-        foreach ($key as $i => $segment) {
-            unset($key[$i]);
-
-            if (is_null($segment)) {
-                return $target;
-            }
-
-            if ($segment === '*') {
-                if ($target instanceof Collection) {
-                    $target = $target->all();
-                } elseif (! is_array($target)) {
-                    return value($default);
-                }
-
-                $result = [];
-
-                foreach ($target as $item) {
-                    $result[] = data_get($item, $key);
-                }
-
-                return in_array('*', $key) ? Arr::collapse($result) : $result;
-            }
-
-            if (Arr::accessible($target) && Arr::exists($target, $segment)) {
-                $target = $target[$segment];
-            } elseif (is_object($target) && isset($target->{$segment})) {
-                $target = $target->{$segment};
-            } else {
+        if ($segment === '*') {
+            if ($target instanceof Collection) {
+                $target = $target->all();
+            } elseif (! is_array($target)) {
                 return value($default);
             }
+
+            $result = [];
+
+            foreach ($target as $item) {
+                $result[] = data_get($item, $key);
+            }
+
+            return in_array('*', $key) ? Arr::collapse($result) : $result;
         }
 
-        return $target;
-    }
-}
-
-if (! function_exists('data_set')) {
-    /**
-     * Set an item on an array or object using dot notation.
-     *
-     * @param  mixed  $target
-     * @param  string|array  $key
-     * @param  mixed  $value
-     * @param  bool  $overwrite
-     * @return mixed
-     */
-    function data_set(&$target, $key, $value, $overwrite = true)
-    {
-        $segments = is_array($key) ? $key : explode('.', $key);
-
-        if (($segment = array_shift($segments)) === '*') {
-            if (! Arr::accessible($target)) {
-                $target = [];
-            }
-
-            if ($segments) {
-                foreach ($target as &$inner) {
-                    data_set($inner, $segments, $value, $overwrite);
-                }
-            } elseif ($overwrite) {
-                foreach ($target as &$inner) {
-                    $inner = $value;
-                }
-            }
-        } elseif (Arr::accessible($target)) {
-            if ($segments) {
-                if (! Arr::exists($target, $segment)) {
-                    $target[$segment] = [];
-                }
-
-                data_set($target[$segment], $segments, $value, $overwrite);
-            } elseif ($overwrite || ! Arr::exists($target, $segment)) {
-                $target[$segment] = $value;
-            }
-        } elseif (is_object($target)) {
-            if ($segments) {
-                if (! isset($target->{$segment})) {
-                    $target->{$segment} = [];
-                }
-
-                data_set($target->{$segment}, $segments, $value, $overwrite);
-            } elseif ($overwrite || ! isset($target->{$segment})) {
-                $target->{$segment} = $value;
-            }
+        if (Arr::accessible($target) && Arr::exists($target, $segment)) {
+            $target = $target[$segment];
+        } elseif (is_object($target) && isset($target->{$segment})) {
+            $target = $target->{$segment};
         } else {
-            $target = [];
-
-            if ($segments) {
-                data_set($target[$segment], $segments, $value, $overwrite);
-            } elseif ($overwrite) {
-                $target[$segment] = $value;
-            }
+            return value($default);
         }
-
-        return $target;
     }
+
+    return $target;
 }
 
-if (! function_exists('tap')) {
-    /**
-     * Call the given Closure with the given value then return the value.
-     *
-     * @param  mixed  $value
-     * @param  callable|null  $callback
-     * @return mixed
-     */
-    function tap($value, $callback = null)
-    {
-        if (is_null($callback)) {
-            return new HigherOrderTapProxy($value);
+/**
+ * Set an item on an array or object using dot notation.
+ *
+ * @param  mixed  $target
+ * @param  string|array  $key
+ * @param  mixed  $value
+ * @param  bool  $overwrite
+ * @return mixed
+ */
+function data_set(&$target, $key, $value, $overwrite = true)
+{
+    $segments = is_array($key) ? $key : explode('.', $key);
+
+    if (($segment = array_shift($segments)) === '*') {
+        if (! Arr::accessible($target)) {
+            $target = [];
         }
 
-        $callback($value);
+        if ($segments) {
+            foreach ($target as &$inner) {
+                data_set($inner, $segments, $value, $overwrite);
+            }
+        } elseif ($overwrite) {
+            foreach ($target as &$inner) {
+                $inner = $value;
+            }
+        }
+    } elseif (Arr::accessible($target)) {
+        if ($segments) {
+            if (! Arr::exists($target, $segment)) {
+                $target[$segment] = [];
+            }
 
-        return $value;
+            data_set($target[$segment], $segments, $value, $overwrite);
+        } elseif ($overwrite || ! Arr::exists($target, $segment)) {
+            $target[$segment] = $value;
+        }
+    } elseif (is_object($target)) {
+        if ($segments) {
+            if (! isset($target->{$segment})) {
+                $target->{$segment} = [];
+            }
+
+            data_set($target->{$segment}, $segments, $value, $overwrite);
+        } elseif ($overwrite || ! isset($target->{$segment})) {
+            $target->{$segment} = $value;
+        }
+    } else {
+        $target = [];
+
+        if ($segments) {
+            data_set($target[$segment], $segments, $value, $overwrite);
+        } elseif ($overwrite) {
+            $target[$segment] = $value;
+        }
     }
+
+    return $target;
+}
+
+/**
+ * Call the given Closure with the given value then return the value.
+ *
+ * @param  mixed  $value
+ * @param  callable|null  $callback
+ * @return mixed
+ */
+function tap($value, $callback = null)
+{
+    if (is_null($callback)) {
+        return new HigherOrderTapProxy($value);
+    }
+
+    $callback($value);
+
+    return $value;
 }
